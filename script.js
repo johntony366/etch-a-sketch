af47126e53758a90375a8b0190cba3f3186fef11
John <johntony366@gmail.com>
2022-07-23 23:07:43 +0530

    Add multi-color draw feature

--- script.js
+++ script.js
@@ -4,24 +4,29 @@ const color = document.querySelector('input');
 
 function createGrid(n) {
     grid.replaceChildren();
+    let drawToggled = false;
     for (let i = 0; i < n**2; ++i) {
         const div = document.createElement('div');
         div.setAttribute('class', 'gridElement')
         div.style.width = `${100 / n}%`;
-        let draw = false;
         div.addEventListener('mousedown', (e) => {
             console.log(e);
-            draw = true;
+            drawToggled = true;
         })
-        div.addEventListener('hover', (e) => {
-            if (true) {
+        div.addEventListener('mouseover', (e) => {
+            if (drawToggled) {
                 console.log(e);
                 div.classList.add('active');
                 div.style.backgroundColor = color.value;
             }
         })
-        div.addEventListener('mouseup', () => {
-            draw = false;
+        div.addEventListener('dragstart', (event) => {
+            event.preventDefault();
+            return false;   
+        })
+        div.addEventListener('mouseup', (e) => {
+            console.log(e);
+            drawToggled = false;
         })
         grid.appendChild(div);
     }
